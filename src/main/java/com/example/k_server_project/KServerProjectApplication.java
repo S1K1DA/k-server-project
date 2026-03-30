9c279a9d79202fc6cec97bd6cfbcea9d44c946da
EUNSIK JUNG <bamtol2100@naver.com>
2026-03-30 12:14:34 +0900

    first commit

---
+++ src/main/java/com/example/k_server_project/KServerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.k_server_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KServerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KServerProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/k_server_project/KServerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k_server_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KServerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
